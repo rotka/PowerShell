e092dd208c10b1eca91b0b14559581d1ea1613e1
Nashav <julien.g6@hotmail.fr>
2019-07-19 11:17:51 +0200

    Signed-off-by: Nashav <julien.g6@hotmail.fr>

--- create_dns_record.psm1
+++ create_dns_record.psm1
@@ -11,30 +11,11 @@ Write-Host "Veuillez Renseigner le Hostname de la machine" -ForegroundColor Cyan
 
 Write-Host "Veuillez Renseigner l'adresse IP de la machine" -ForegroundColor Cyan -NoNewline; $ipESXi = Read-Host " "
 
-$ComputerName = $ipESXi
+$ComputerName = $hostESXi
 
 Write-Host "Veuillez Renseigner la Zone" -ForegroundColor Cyan -NoNewline; $ZoneName = Read-Host " " #"wallmart.esgi"
 
 Add-DnsServerResourceRecordA -ZoneName $ZoneName -Name $hostESXi -IPv4Address $ipESXi | Out-File -FilePath $Outfile -Encoding utf8 -Append
 Write-Host "L'entrée de $hostESXi dans la zone $ZoneName à été créé correctement" -ForegroundColor Green
 
-
-### New Ad Computer
-
-$pc = Get-ADComputer -Filter 'Name -eq $ComputerName'
-
-if ($pc=$ComputerName) {
-
-Remove-ADComputer -Identity $ComputerName | Out-File -FilePath $Outfile -Encoding utf8 -Append
-Write-Host "L'ancien Poste à été supprimé" -ForegroundColor Green
-
-New-ADComputer -Name "$ComputerName" -Path "OU=Ordinateurs,DC=wallmart,DC=esgi" | Out-File -FilePath $Outfile -Encoding utf8 -Append
-Write-Host "Le Nouveau Poste à été créé" -ForegroundColor Green
-
-}else {
-
-New-ADComputer -Name "$ComputerName" -Path "OU=Ordinateurs,DC=wallmart,DC=esgi" | Out-File -FilePath $Outfile -Encoding utf8 -Append
-Write-Host "Le Nouveau Poste à été créé" -ForegroundColor Green
-
-}
 }
